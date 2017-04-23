b4bb713fba2527a8ce5359283d5c020f3e60270c
Arjo Bruijnes <arjo@weirdwater.net>
2017-04-23 22:54:37 +0200

    Print list of participants

--- src/components/container/ListViewContainer.jsx
+++ src/components/container/ListViewContainer.jsx
@@ -3,10 +3,19 @@ import base from '../../base'
 
 class ListViewContainer extends React.Component {
 
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      participants: {}
+    }
+
+  }
+
   componentWillMount() {
     this.ref = base.syncState('/', {
       context: this,
-      state: 'signups'
+      state: 'participants'
     });
   }
 
@@ -14,10 +23,33 @@ class ListViewContainer extends React.Component {
     base.removeBinding(this.ref)
   }
 
+  renderParticipant(participant, key) {
+    const {signup} = participant
+
+    return (
+      <tbody key={key}>
+        <tr>
+          <td>{signup.name} {signup.surnamePrefix} {signup.surname}</td>
+          <td>{signup.email}</td>
+          <td>{signup.phonenumber}</td>
+        </tr>
+      </tbody>
+    )
+  }
+
   render() {
     return (
       <div>
-        ListViewContainer
+        <table>
+          <tbody>
+            <tr>
+              <th>Naam</th>
+              <th>E-mailadres</th>
+              <th>Telefoonnummer</th>
+            </tr>
+          </tbody>
+          {Object.keys(this.state.participants).map(key => this.renderParticipant(this.state.participants[key], key))}
+        </table>
       </div>
     )
   }
