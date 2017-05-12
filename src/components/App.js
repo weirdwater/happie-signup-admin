711db6c24973294ad7698fce36739cafc8927a55
Arjo Bruijnes <arjo@weirdwater.net>
2017-05-12 13:26:55 +0200

    Add authentication to the application to protect data

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,42 @@ import React, { Component } from 'react';
 import styles from './App.css'
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom'
-import ListViewContainer from "./container/ListViewContainer";
-import DetailViewContainer from "./container/DetailViewContainer";
+import ListViewContainer from './container/ListViewContainer';
+import DetailViewContainer from './container/DetailViewContainer';
+import LoginPage from './presentational/LoginPage'
+
 
 class App extends Component {
 
+  constructor(props) {
+    super(props)
+
+    this.authenticate = this.authenticate.bind(this)
+    this.logOut = this.logOut.bind(this)
+
+    this.state = {
+      authenticated: false
+    }
+  }
+
+  authenticate() {
+    this.setState({authenticated: true})
+  }
+
+  logOut() {
+    window.localStorage.removeItem('uid')
+    this.setState({authenticated: false})
+  }
+
   render() {
+    if (!this.state.authenticated) {
+      return <LoginPage authenticate={this.authenticate} />
+    }
+
     return (
       <BrowserRouter>
           <main className={styles.app}>
+              <button onClick={this.logOut} className={styles.logout}>Uitloggen</button>
               <Route exact path="/" component={ListViewContainer} />
               <Route path="/:id" component={DetailViewContainer} />
             <Route/>
---
+++ src/components/presentational/LoginPage.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './LoginPage.css'
+import base from '../../base'
+
+class LoginPage extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.authHandler = this.authHandler.bind(this)
+    this.handleLoginButton = this.handleLoginButton.bind(this)
+    this.userIsPrivileged = this.userIsPrivileged.bind(this)
+
+    this.state = {
+      authenticating: false,
+      showNotPrivileged: false,
+      error: false,
+      adminMail: 'arjo@weirdwater.net',
+      uid: ''
+    }
+
+    this.privilegedUsers = [
+      'HzLGQFfoy0PlbjCiTOntAv4VCNS2'
+    ]
+  }
+
+  componentWillMount() {
+    const uid = window.localStorage.getItem('uid')
+    if(uid !== null) {
+      if (this.userIsPrivileged(uid)) {
+        this.props.authenticate();
+      }
+      else {
+        this.setState({uid})
+      }
+    }
+  }
+
+  userIsPrivileged(uid) {
+    return this.privilegedUsers.filter(privUid => privUid === uid).length === 1
+  }
+
+  authHandler(error, user) {
+    const nextState = {authenticating: false}
+
+    if (error) {
+      console.debug(error)
+      nextState.error = error
+    }
+    else {
+      const uid = user.user.uid
+      nextState.uid = uid
+
+      if (this.userIsPrivileged(uid)) {
+        window.localStorage.setItem('uid', uid)
+        this.props.authenticate()
+        return
+      }
+      else {
+        nextState.showNotPrivileged = true
+      }
+    }
+
+    this.setState(nextState)
+  }
+
+  handleLoginButton(event) {
+    const provider = event.target.name
+    base.authWithOAuthPopup(provider, this.authHandler)
+    this.setState({authenticating: true})
+  }
+
+  render() {
+    return (
+      <section className={styles.container} >
+        <h1>Log in</h1>
+        <button className={styles.facebook} name="facebook" onClick={this.handleLoginButton}>Log in with Facebook</button>
+        <button className={styles.google} name="google" onClick={this.handleLoginButton}>Log in with Google</button>
+        {this.state.showNotPrivileged ? <p className={styles.message} >Je hebt geen bevoegdheid deze pagina te bekijken. Heb je dat wel nodig? Stuur dan een <a href={`mailto:${this.state.adminMail}?subject=Happy%20Signup%20Admin%20access%20request&body=User%20id:%20${this.state.uid}`}>mailtje</a>.</p> : ''}
+        {this.state.error ? <p className={styles.error} ><strong>Oops!</strong> Er is iets fout gegaan met het inloggen, probeer het later nog eens. Stuur een <a href={`mailto:${this.state.adminMail}?subject=Happy%20Signup%20Admin%20login%20bug%20report&body=${this.state.error}`}>mailtje</a> als dit vaker gebeurt.</p> : ''}
+      </section>
+    )
+  }
+}
+
+LoginPage.propTypes = {
+  authenticate: PropTypes.func.isRequired
+}
+
+export default LoginPage
